a969ccab33083b64b11a94a84eb71f670d702c10
dhruvdk3 <dhruvkumawat333@gmail.com>
2024-09-27 16:18:47 +0530

    deletion

--- Tree/BST.cpp
+++ Tree/BST.cpp
@@ -30,6 +30,58 @@ void preorder(Node* root){
     preorder(root->right);
 }
 
+Node* minval(Node* root){
+    while (root->left != NULL) root = root->left;
+    return root;
+}
+
+Node* deleteNode(Node*& root, int val){
+    if (root == NULL) return root;
+    if(root->data == val){
+        // 0 child 
+        if(root->right == NULL && root->left == NULL){
+            delete root;
+            return NULL;
+        }
+
+        // 1 child 
+        if(root->right == NULL && root->left != NULL){
+            Node* temp = root->left;
+            delete root;
+            return temp;
+        }
+        if(root->right != NULL && root->left == NULL){
+            Node* temp = root->right;
+            delete root;
+            return temp;
+        }
+
+        // 2 CHILD 
+        if(root->right != NULL && root->left != NULL){
+            int mini = minval(root->right)->data;
+            root->data = mini;
+            root->right = deleteNode(root->right, mini);
+            return root;
+        }
+    }
+    else if(root->data > val){
+        root->left = deleteNode(root->left, val);
+        return root;
+    }
+    else {
+        root->right = deleteNode(root->right, val);
+        return root;
+    }
+    return root;
+}
+
+void inorder(Node* root){
+    if(root == NULL) return;
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+}
+
 int main(){
     Node* root = new Node(5);
     insert(root, 2);
@@ -40,5 +92,12 @@ int main(){
     insert(root, 21);
     insert(root, 19);
     insert(root, 25);
+    preorder(root);
+    cout<<endl;
+    inorder(root);
+    deleteNode(root, 9);
+    cout<<endl;
+    inorder(root);
+    cout<<endl;
     preorder(root);
 }
