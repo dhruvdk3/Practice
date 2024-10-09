67f6d92108d327f1559d9ca0597e36400353de19
dhruvdk3 <dhruvkumawat333@gmail.com>
2024-10-09 22:47:41 +0530

    delete

--- Heap/heap.cpp
+++ Heap/heap.cpp
@@ -21,8 +21,36 @@ class Heap{
             }
         }
 
+
+
+        void deletion(){
+            if(size == 0) {
+                cout<<"Nothing to delete";
+                return;
+            }
+            arr[1] = arr[size--];
+            int i = 1;
+            while(i<size){
+                int leftInd = 2*i;
+                int righttInd = 2*i+1;
+                if(leftInd <size && arr[i]<arr[leftInd]) {
+                    swap(arr[leftInd], arr[i]);
+                    i = leftInd;
+                }
+                else if(righttInd <size && arr[i]<arr[righttInd]){
+                    swap(arr[righttInd], arr[i]);
+                    i = righttInd;
+                }
+                else return;
+            }
+        }
+
+
+
+
         void print(){
             for(int i = 1; i<=size; i++) cout<<arr[i]<<" ";
+            cout<<endl;
         }
 };
 
@@ -33,6 +61,8 @@ int main(int argc, char const *argv[]){
     p.insert(40);
     p.insert(30);
     p.insert(20);
+    p.print();
+    p.deletion();
     p.print();
     return 0;
 }
